6455bc238e3c85f76b27a0a92700a9594de8691c
Mike Daley <michaelcdaley@yahoo.com>
2018-02-27 14:10:40 -0800

    Added description validation test for add_transaction.js

--- test/javascript/transactions/add_transaction.test.js
+++ test/javascript/transactions/add_transaction.test.js
@@ -19,9 +19,11 @@ import AddTransaction       from '../../../app/javascript/components/transaction
 //    1.) Simulate AJAX response using a sinon.stub()
 //    2.) Verify the empty fields have class '.has-error'
 // [] Test clicking 'Add' with fields, should receive new transaction 
+//    1.) I CAN'T DO THIS RIGHT NOW AS MY AJAX CODE IS EMBEDDED IN THE
+//        handleSubmit CALLBACK.
 ///////////////////////////////////////////////////////////////////////////////
 
-
+// Test snapshot view of AddTransaction
 it('renders correctly', () => {
   const handleSubmitMock = jest.fn()
   
@@ -31,13 +33,32 @@ it('renders correctly', () => {
   expect(tree).toMatchSnapshot()
 });
 
-/**
+
+// Test filling in transaction description
+it('handles description input', () => {
+  // given
+  const handleSubmitMock  = jest.fn()
+  const wrapper           = shallow(<AddTransaction addTransaction={handleSubmitMock} />);
+  const form              = wrapper.find('#txnDescription');
+  // when
+  form.props().onChange({target: {
+    name:   'description',
+    value:  'myValue'
+  }});
+  // then
+  expect(wrapper.state('description')).toEqual('myValue');
+})
+
+///////////////////////////////////////////////////////////////////////////////
+// TODO: 02/26/2018
+// -  BUG WITH ACCESSING THE DOCUMENT OBJECT THAT I USE TO GET THE CSRF TOKEN,
+//    WHEN I'M TESTING JUST A COMPONENT THEN THERE WILL NOT BE A DOCUMENT
+///////////////////////////////////////////////////////////////////////////////
+/****
 it('handles submit', () => {
   const handleSubmitMock  = jest.fn()
   const wrapper           = shallow(<AddTransaction addTransaction={handleSubmitMock} />)
   wrapper.find('.btn-submit').simulate('click', { preventDefault: () => {} })
   expect(handleSubmitMock.mock.calls.length).toBe(1)
 })
-**/
-
-
+****/
